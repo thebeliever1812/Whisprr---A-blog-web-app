b8b8fec6ea20817a741e2babffffc775b78db3ed
Basir Ahmad <basirahmadmalik@gmail.com>
2025-05-14 13:11:10 +0530

    some basic bug fix

--- src/pages/MyBlogs.jsx
+++ src/pages/MyBlogs.jsx
@@ -74,10 +74,10 @@ function MyBlogs() {
 										>
 											<path
 												d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
-												stroke-width="1.5"
+												strokeWidth="1.5"
 											></path>
-											<path d="M8 12H16" stroke-width="1.5"></path>
-											<path d="M12 16V8" stroke-width="1.5"></path>
+											<path d="M8 12H16" strokeWidth="1.5"></path>
+											<path d="M12 16V8" strokeWidth="1.5"></path>
 										</svg>
 									</Link>
 								</div>

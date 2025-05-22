cafbf78c93d22c3feee4bf06653a093210be7026
Basir Ahmad <basirahmadmalik@gmail.com>
2025-05-22 22:12:04 +0530

    updated lazyloader css

--- src/components/LazyLoader.jsx
+++ src/components/LazyLoader.jsx
@@ -1,12 +1,12 @@
 function LazyLoader() {
 	return (
-		<div class="flex flex-col bg-neutral-300 w-56 h-64 animate-pulse rounded-xl p-4 gap-4">
-			<div class="bg-neutral-400/50 w-full h-32 animate-pulse rounded-md"></div>
-			<div class="flex flex-col gap-2">
-				<div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-				<div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-				<div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-				<div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+		<div className="flex flex-col bg-neutral-300 w-full h-64 animate-pulse rounded-xl sm:p-5 p-4 gap-4">
+			<div className="bg-neutral-400/50 w-full h-32 animate-pulse rounded-md"></div>
+			<div className="flex flex-col gap-2">
+				<div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+				<div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+				<div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+				<div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
 			</div>
 		</div>
 	);
